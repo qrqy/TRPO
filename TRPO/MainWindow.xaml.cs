85e51325b5c8702257b7b69171861b0f5ec8958c
qrqy <108899327+qrqy@users.noreply.github.com>
2023-03-24 10:32:02 +0300

    Добавьте файлы проекта.

---
+++ TRPO/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using TRPO.Pages; //Подключение простанства Pages
+
+namespace TRPO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new AuthPage());
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Вы уверены, что хотите закрыть программу?", "Окно закрытия", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No) { e.Cancel=true; }
+            else { e.Cancel = false; }
+        }
+    }
+}
---
+++ TRPO/Pages/AuthPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TRPO.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        
+
+
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            academEntities ent = new academEntities();
+            var req = from x in ent.Auth
+                      where x.Login == Login.Text
+                      select x.Password;
+
+
+            if (req.Count() > 0 && req.ToList().Contains(Password.Password.ToString()))
+            {
+                mainWindow.dbPage.Navigate(new MenuPage(Login.Text));
+            }
+            else
+            {
+                MessageBox.Show("Не верный логин или пароль");
+
+                Password.Clear();
+            }
+            */
+        }
+
+
+        private void Login_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Password.Focus();
+            }
+
+        }
+
+        private void Password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                LoginButton.Focus();
+            }
+        }
+        private void ShowPassword_PreviewMouseDown(object sender, MouseButtonEventArgs e) => ShowPasswordFunction();
+        private void ShowPassword_PreviewMouseUp(object sender, MouseButtonEventArgs e) => HidePasswordFunction();
+        private void ShowPassword_MouseLeave(object sender, MouseEventArgs e) => HidePasswordFunction();
+        private void ShowPasswordFunction()
+        {
+            ShowPassword.Text = "HIDE";
+            //dockPanel.Visibility = Visibility.Visible;
+            Password.Visibility = Visibility.Hidden;
+            PasswordUnmask.Text = Password.Password;
+
+        }
+
+        private void HidePasswordFunction()
+        {
+            ShowPassword.Text = "SHOW";
+            //dockPanel.Visibility = Visibility.Hidden;
+            Password.Visibility = Visibility.Visible;
+
+        }
+
+        private void LoginButton_PreviewMouseDown(object sender, MouseButtonEventArgs e) => LoginButton_Event();
+        private void LoginButton_Event()
+        {
+            MessageBox.Show("Для регистрации обратитесь к администратору");
+        }
+
+        private void Login_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
